1483790ca5a73ff9951aa27af34999da9c823b27
karhel <karheltmarr@hotmail.fr>
2025-03-21 21:23:44 +0100

    Install Bootstrap CSS

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -3,3 +3,6 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap';
